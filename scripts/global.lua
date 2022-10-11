811abef57a939f4afdcc698db390019eff48db18
selimanac <selimanac@gmail.com>
2022-10-11 23:10:15 +0300

    no message

--- scripts/global.lua
+++ scripts/global.lua
@@ -1,22 +1,43 @@
 local global = {}
 
-global.screen_width = 240
-global.screen_height = 160
-global.spaceship = {url = msg.url(), sprite = msg.url(), thruster = msg.url(), pos = vmath.vector3(), rot = vmath.vector3()}
+global.proxy_url = ""
+global.hud_url = ""
+
+global.shield_v = 0
+global.life_v = 0
+
+global.screen_width = 480
+global.screen_height = 270
+global.spaceship = {url = msg.url(), sprite = msg.url(), thruster = msg.url(), pos = vmath.vector3(), rot = vmath.vector3(), enemy_aabb_id = 0, enemy_missile_aabb_id = 0,collectable_aabb_id = 0}
 
 global.vector_up = vmath.vector3(0, 1, 0)
 
-global.move = {LEFT = hash("move_left"), RIGHT = hash("move_right"), UP = hash("move_up"), DOWN = hash("move_down")}
+global.move = {LEFT = hash("move_left"), RIGHT = hash("move_right"), UP = hash("move_up"), DOWN = hash("move_down"), LASER = hash("fire_laser")}
 
 global.enemy_group_id = -1
+global.enemy_missile_group_id = -1
+global.collectable_group_id = -1
+
 global.enemies = {}
-
 global.missiles = {}
+global.enemy_missiles = {}
+global.collectables = {}
 
-local factories = {ENEMY = msg.url("main:/factories#enemy"), EXPLODE = msg.url("main:/factories#explode")}
+global.isPaused = false
+
+global.elapsed_time_m = 0 
+global.elapsed_time_s = 0 
+
+global.factories = {
+    ENEMY = msg.url("main:/factories#enemy"),
+    EXPLODE = msg.url("main:/factories#explode"),
+    MISSILE = msg.url("main:/factories#ship_projectile"),
+    ENEMY_MISSILE = msg.url("main:/factories#enemy_projectile"),
+    COLLECTABLE = msg.url("main:/factories#collectable")
+}
 
 local enemy_timer = function()
-    timer.delay(rnd.double_range(0.1, 0.3), false, global.dispatch_enemy)
+    timer.delay(rnd.double_range(0.1, 0.2), false, global.dispatch_enemy)
 end
 
 local function remove_enemy(self, url, property)
@@ -24,8 +45,8 @@ local function remove_enemy(self, url, property)
 end
 
 function global.dispatch_enemy()
-    local p
-    local endpos
+    local p =vmath.vector3()
+    local endpos =vmath.vector3()
 
     if rnd.toss() == 1 then
         if rnd.toss() == 1 then
@@ -46,25 +67,49 @@ function global.dispatch_enemy()
     end
 
     local angle = math.atan2(p.x - endpos.x, endpos.y - p.y)
-    local enemy = factory.create(factories.ENEMY, p, vmath.quat_rotation_z(angle))
-    go.animate(enemy, "position", go.PLAYBACK_ONCE_FORWARD, endpos, go.EASING_LINEAR, rnd.range(6, 20), 0, remove_enemy)
+    local enemy = factory.create(global.factories.ENEMY, p, vmath.quat_rotation_z(angle))
+    go.animate(enemy, "position", go.PLAYBACK_ONCE_FORWARD, endpos, go.EASING_LINEAR, rnd.range(5, 20), 0, remove_enemy)
 
     enemy_timer()
 end
 
 function global.init()
     global.enemy_group_id = aabb.new_group()
+    global.enemy_missile_group_id = aabb.new_group()
+    global.collectable_group_id = aabb.new_group()
     enemy_timer()
 end
 
 function global.enemy_explode(pos, id)
-    local explode = factory.create(factories.EXPLODE, pos)
+    local explode = factory.create(global.factories.EXPLODE, pos)
 
     sprite.play_flipbook(explode, "enemy_impact", function()
         go.delete(explode)
-        go.delete(id)
+        if id then
+            go.delete(id)
+        end
     end)
+end
 
+function global.pause_game()
+    if global.isPaused == false then
+        msg.post(global.proxy_url, "set_time_step", {factor = 0, mode = 1})
+        global.isPaused = true
+        aabb.run(false)
+    else
+        msg.post(global.proxy_url, "set_time_step", {factor = 1, mode = 1})
+        global.isPaused = false
+        aabb.run(true)
+    end
+
+end
+
+function global.dispacth_collectable(pos)
+
+    local collectable = factory.create(global.factories.COLLECTABLE, pos,vmath.quat(0, 0, 0, 0))
+ 
+
+ 
 end
 
 return global
--- scripts/spaceship.lua
+++ scripts/spaceship.lua
@@ -10,21 +10,31 @@ local rotation_speed = 180
 local input = vmath.vector3()
 local speed = 0
 local missile_move_amount = g.vector_up * 400
-local missile_factory = msg.url("main:/factories#ship_projectile")
+
 local rot_amount
 local move_amount
+
+local impact = false
+
+local ray_end = vmath.vector3(100, 100, 0)
+
+local ship_hit_result = {}
+local ship_missile_hit_result = {}
+local ray_result = {}
+local collectable_result = {}
 
 local function delete_projectile(self, url, property)
     go.delete(url)
 end
 local function fire_projectile()
-    local missile = factory.create(missile_factory, g.spaceship.pos, g.spaceship.rot)
+    local missile = factory.create(g.factories.MISSILE, g.spaceship.pos, g.spaceship.rot)
     local target = g.spaceship.pos + vmath.rotate(g.spaceship.rot, missile_move_amount)
 
-    go.animate(missile, "position", go.PLAYBACK_ONCE_FORWARD, target, go.EASING_LINEAR, 6, 0, delete_projectile)
+    go.animate(missile, "position", go.PLAYBACK_ONCE_FORWARD, target, go.EASING_LINEAR, 3, 0, delete_projectile)
 end
 
 function ship.init()
+    msg.post("/laser", "disable")
     g.spaceship.url = msg.url("/spaceship")
     g.spaceship.sprite = msg.url("/spaceship#ship")
 
@@ -37,12 +47,39 @@ function ship.init()
     go.set_position(vmath.vector3(g.spaceship.pos.x, g.spaceship.pos.y, g.spaceship.pos.z), g.spaceship.url)
     go.set(g.spaceship.sprite, "cursor", 0.5)
 
+    g.spaceship.enemy_aabb_id = aabb.insert_gameobject(g.enemy_group_id, g.spaceship.url, 10, 10)
+    g.spaceship.enemy_missile_aabb_id = aabb.insert_gameobject(g.enemy_missile_group_id, g.spaceship.url, 10, 10)
+    g.spaceship.collectable_aabb_id = aabb.insert_gameobject(g.collectable_group_id, g.spaceship.url, 10, 10)
+
     input = vmath.vector3()
     speed = 0
-    timer.delay(0.15, true, fire_projectile)
+    timer.delay(0.12, true, fire_projectile)
+end
+
+local function dispatch_ray()
+    -- go.set("/laser#sprite", "size.y", 20)
+    msg.post("/laser", "enable")
+    ray_end = g.spaceship.pos + vmath.rotate(g.spaceship.rot, g.vector_up * 70)
+
+    ray_result = aabb.raycast(g.enemy_group_id, g.spaceship.pos.x, g.spaceship.pos.y, ray_end.x, ray_end.y)
+
+    if ray_result then
+        for i = 1, #ray_result do
+            if g.enemies[ray_result[i]] then
+                msg.post(g.enemies[ray_result[i]].url, "take_damage")
+            end
+        end
+    end
 end
 
 function ship.input(action_id, action)
+    if action_id == g.move.LASER then
+        dispatch_ray()
+    end
+
+    if action_id == g.move.LASER and action.released then
+        msg.post("/laser", "disable")
+    end
 
     if action_id == g.move.UP then
         input.y = 1
@@ -61,7 +98,79 @@ function ship.input(action_id, action)
     end
 end
 
-function ship.update(dt)
+local function hit_anim()
+    if g.shield_v.x > 0 then
+
+        if impact == false then
+            msg.post(g.hud_url, "take_damage")
+            impact = true
+            sprite.play_flipbook("/shield#sprite", "ship_shield", function()
+                impact = false
+            end)
+        end
+
+    else
+
+        if impact == false then
+            msg.post(g.hud_url, "take_damage")
+            impact = true
+            local explode = factory.create(g.factories.EXPLODE, g.spaceship.pos)
+            -- go.set_parent(explode, g.spaceship.url)
+            sprite.play_flipbook(explode, "enemy_impact", function()
+                go.delete(explode)
+                impact = false
+                if g.life_v.x <= 0 then
+                    msg.post(g.proxy_url, "the_end")
+                end
+            end)
+        end
+
+    end
+end
+
+local function check_enemy_missile_hit()
+
+    ship_missile_hit_result = aabb.query_id(g.enemy_missile_group_id, g.spaceship.enemy_missile_aabb_id)
+
+    if ship_missile_hit_result then
+        for i = 1, #ship_missile_hit_result do
+            if g.enemy_missiles[ship_missile_hit_result[i]] then
+                go.delete(g.enemy_missiles[ship_missile_hit_result[i]].url)
+                hit_anim()
+            end
+        end
+    end
+end
+
+local function check_enemy_hit()
+
+    ship_hit_result = aabb.query_id(g.enemy_group_id, g.spaceship.enemy_aabb_id)
+
+    if ship_hit_result then
+        for i = 1, #ship_hit_result do
+            if g.enemies[ship_hit_result[i]] then
+                msg.post(g.enemies[ship_hit_result[i]].url, "take_damage")
+                hit_anim()
+            end
+        end
+    end
+end
+
+local function check_collectable()
+    collectable_result = aabb.query_id(g.collectable_group_id, g.spaceship.collectable_aabb_id)
+
+    if collectable_result then
+        for i = 1, #collectable_result do
+            if g.collectables[collectable_result[i]] and g.collectables[collectable_result[i]].active then
+                g.collectables[collectable_result[i]].active = false
+                msg.post(g.hud_url, "collect")
+                msg.post(g.collectables[collectable_result[i]].url, "collect")
+            end
+        end
+    end
+end
+
+local function ship_position(dt)
 
     if input.y > 0 then
         speed = speed + acceleration * dt
@@ -93,7 +202,19 @@ function ship.update(dt)
 
     go.set_position(g.spaceship.pos, g.spaceship.url)
 
+    go.set_position(vmath.vector3(-1*(g.spaceship.pos.x/10),-1*(g.spaceship.pos.y/10), 0), "/back")
+
     input = vmath.vector3()
+end
+
+function ship.update(dt)
+
+    ship_position(dt)
+
+    check_enemy_hit()
+    check_enemy_missile_hit()
+
+    check_collectable()
 
 end
 
